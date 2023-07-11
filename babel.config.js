a501a223f1e10a1698b01acfaff92811137b18d2
Vítor Emanuel Queiroz Ferreira <vitoremanuelqf@gmail.com>
2023-07-10 23:33:39 -0300

    Atualização das dependências.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@contexts': './src/contexts',
+            '@hooks': './src/hooks',
+            '@locales': './src/locales',
+            '@routes': './src/routes',
+            '@schemas': './src/schemas',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@storage': './src/storage',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+
+      'react-native-reanimated/plugin',
+    ],
   };
 };
